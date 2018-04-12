0345fc4ddb2017bb3d931d6e19ad60d76e689a21
mohit121312 <31860988+mohit121312@users.noreply.github.com>
2018-04-12 22:25:18 +0530

    Add files via upload

---
+++ facebook/confi/auth.js
@@ -0,0 +1,7 @@
+module.export = {
+	"facebookAuth":{
+		"clientId":  " 377491846067607 ",
+		"clientSecret" : "d52e0171d5932c37f1ded08ee4f843a6 ",
+		"callbackUrl":'http://localhost:8080/auth/facebook/callback'
+	}
+}
